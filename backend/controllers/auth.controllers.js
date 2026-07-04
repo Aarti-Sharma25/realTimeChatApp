cce7508fb6f4dd040ce43ad9bf4a9b0f4952e715
Aarti Sharma <aartisharmaindian25@gmail.com>
2026-07-04 13:40:58 +0530

    Change cookie settings for token in auth controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -26,8 +26,8 @@ export const signup=async(req,res)=>{
    res.cookie("token",token,{
     httpOnly:true,
     maxAge:7*24*60*60*1000,
-    sameSite:"None",
-    secure:true
+    sameSite:"lax",
+    secure:false
    }
 
    )
