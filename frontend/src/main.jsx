194bef2538875206c495b7bffe8b00cdb979290e
Aarti Sharma <aartisharmaindian25@gmail.com>
2026-09-01 14:17:19 +0530

    Update serverUrl to production URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,8 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
-//export const serverUrl="https://realtimechatapp-backend-lkmh.onrender.com"
-export const serverUrl = "http://localhost:8000";
+export const serverUrl="https://realtimechatapp-backend-lkmh.onrender.com"
+//export const serverUrl = "http://localhost:8000";
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Provider store={store}>
